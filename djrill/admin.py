59131f2ad208da196cf7ef27fea6cf8bc98103ea
Rich Atkinson <rich@piran.com.au>
2012-09-14 12:06:43 +1000

    make the customised admin site genuinely optional.

--- djrill/admin.py
+++ djrill/admin.py
@@ -5,18 +5,21 @@ from djrill.views import (DjrillIndexView, DjrillSendersListView,
                           DjrillAddSenderView, DjrillTagListView,
                           DjrillUrlListView)
 
-admin.site.register_view("djrill/senders/", DjrillSendersListView.as_view(),
-    "djrill_senders", "senders")
-admin.site.register_view("djrill/status/", DjrillIndexView.as_view(),
-    "djrill_status", "status")
-admin.site.register_view("djrill/tags/", DjrillTagListView.as_view(),
-    "djrill_tags", "tags")
-admin.site.register_view("djrill/urls/", DjrillUrlListView.as_view(),
-    "djrill_urls", "urls")
+if hasattr('admin.site', 'register_view'):
+    # Only do this if the user has registered DjrilAdmin as admin.site
+    admin.site.register_view("djrill/senders/", DjrillSendersListView.as_view(),
+        "djrill_senders", "senders")
+    admin.site.register_view("djrill/status/", DjrillIndexView.as_view(),
+        "djrill_status", "status")
+    admin.site.register_view("djrill/tags/", DjrillTagListView.as_view(),
+        "djrill_tags", "tags")
+    admin.site.register_view("djrill/urls/", DjrillUrlListView.as_view(),
+        "djrill_urls", "urls")
 
-admin.site.register_url("djrill/disable/sender/",
-    DjrillDisableSenderView.as_view(), "djrill_disable_sender")
-admin.site.register_url("djrill/verify/sender/",
-    DjrillVerifySenderView.as_view(), "djrill_verify_sender")
-admin.site.register_url("djrill/add/sender/",
-    DjrillAddSenderView.as_view(), "djrill_add_sender")
+if hasattr('admin.site', 'register_view'):
+    admin.site.register_url("djrill/disable/sender/",
+        DjrillDisableSenderView.as_view(), "djrill_disable_sender")
+    admin.site.register_url("djrill/verify/sender/",
+        DjrillVerifySenderView.as_view(), "djrill_verify_sender")
+    admin.site.register_url("djrill/add/sender/",
+        DjrillAddSenderView.as_view(), "djrill_add_sender")
